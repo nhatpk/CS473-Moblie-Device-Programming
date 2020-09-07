c71cbfbf65cef1621d05c64f4ad3597a7d8e527c
Andy <nhat.phamkhac@gmail.com>
2020-09-06 18:42:30 -0700

    update Table

--- HW3/TableLayout/app/src/main/java/com/example/tablelayout/MainActivity.kt
+++ HW3/TableLayout/app/src/main/java/com/example/tablelayout/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.example.tablelayout
 
+import android.graphics.Color
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.TableLayout
 import android.widget.TableRow
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
         btnAdd.setOnClickListener {
             // Create a new table row.
@@ -25,11 +22,11 @@ class MainActivity : AppCompatActivity() {
             tableRow.layoutParams = layoutParams
 
             // add values into row by calling addView()
-            tableRow.addView(strAndroidVersion,1)
-            tableRow.addView(strCodeName,2)
+            tableRow.addView(strAndroidVersion, 1)
+            tableRow.addView(strCodeName, 2)
 
             // Finally add the created row row into layout
-            tbContent.addView(tableRow) // id from Layout_file
+            tlContent.addView(tableRow) // id from Layout_file
         }
     }
 }
